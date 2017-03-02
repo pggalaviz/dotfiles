ef8f359c37562bbaddc20a01ebd3b848469b8a97
PGGalaviz <pggalaviz@gmail.com>
2017-03-02 12:50:58 -0600

    update

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@
 
 # fish
 mkdir -p ~/.config/fish
+mkdir -p ~/.config/fish/functions
+ln -sf `pwd`/fish/config.fish ~/.config/fish
+ln -sf `pwd`/fish/functions/fish_prompt.fish ~/.config/fish/functions
 
 # hyper
 ln -sf `pwd`/hyper/hyper.js ~/.hyper.js
