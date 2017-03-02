b36061001f949e7d3811109f38077a866902df6d
PGGalaviz <pggalaviz@gmail.com>
2017-03-02 14:27:08 -0600

    updates

--- hyper/hyper.js
+++ hyper/hyper.js
@@ -4,19 +4,19 @@ module.exports = {
     fontSize: 14,
 
     // font family with optional fallbacks
-    fontFamily: '"Source Code Pro for Powerline", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+    fontFamily: 'Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-    cursorColor: 'rgba(248,28,229,0.8)',
+    cursorColor: 'rgba(248,28,229,0.6)',
 
     // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
     cursorShape: 'BLOCK',
 
     // color of the text
-    foregroundColor: '#fff',
+    foregroundColor: '#f8f8f0',
 
     // terminal background color
-    backgroundColor: '#121212',
+    backgroundColor: 'rgba(0,0,0,0.6)',
 
     // border color (window, tabs)
     borderColor: '#333',
@@ -28,33 +28,31 @@ module.exports = {
     termCSS: '',
 
     // custom padding (css format, i.e.: `top right bottom left`)
-    padding: '0',
+    padding: '5px 5px',
 
     // the full list. if you're going to provide the full color palette,
     // including the 6 x 6 color cubes and the grayscale map, just provide
     // an array here instead of a color map object
     colors: {
-      black: '#000000',
-      red: '#ff0000',
-      green: '#33ff00',
-      yellow: '#ffff00',
-      blue: '#0066ff',
-      magenta: '#cc00ff',
-      cyan: '#00ffff',
-      white: '#d0d0d0',
-      lightBlack: '#808080',
-      lightRed: '#ff0000',
-      lightGreen: '#33ff00',
-      lightYellow: '#ffff00',
-      lightBlue: '#0066ff',
-      lightMagenta: '#cc00ff',
-      lightCyan: '#00ffff',
-      lightWhite: '#ffffff'
+      black: '#272822',
+      red: '#f92672',
+      green: '#a6e22e',
+      yellow: '#fd971f',
+      blue: '#ae81ff',
+      magenta: '#f92672',
+      cyan: '#66d9ef',
+      white: '#f8f8f0',
+      lightBlack: '#49483e',
+      lightRed: '#f92672',
+      lightGreen: '#a6e22e',
+      lightYellow: '#e6db74',
+      lightBlue: '#ae81ff',
+      lightMagenta: '#f92672',
+      lightCyan: '#66d9ef',
+      lightWhite: '#f8f8f2'
     },
 
     // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
-    // if left empty, your system's login shell will be used by default
-    //shell: '',
     shell: '/usr/local/bin/fish',
 
     // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
@@ -70,24 +68,17 @@ module.exports = {
     // if true, selected text will automatically be copied to the clipboard
     copyOnSelect: false,
 
-    // URL to custom bell
-    // bellSoundURL: 'http://example.com/bell.mp3',
-
-    // for advanced config flags please refer to https://hyper.is/#cfg
 
     // shortcut to summon hyperterm
     summonShortcut: 'Shift+Cmd+Space',
   },
 
-  // a list of plugins to fetch and install from npm
-  // format: [@org/]project[#version]
-  // examples:
-  //   `hyperpower`
-  //   `@company/project`
-  //   `project#1.0.1`
+  // list of plugins to fetch and install from npm
   plugins: [
     'hyper-font-smoothing', // anti-aliased fonts
     'hypercwd', // open new tabs in the same cwd as current tab
+    'hyperterm-summon', // summon hyperterm, system-wide, with a hotkey
+    'hyperlinks'
   ],
 
   // in development, you can create a directory under
