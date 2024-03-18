29899d2b70bb217af30e6fe6c3b7e26978a8726a
Pedro G. Galaviz <pedro.galaviz@withparallax.com>
2024-03-18 17:46:23 -0600

    Add more keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -27,3 +27,24 @@ vim.api.nvim_set_keymap("i", "<A-down>", "<esc><cmd>m .+1<cr>==gi", { desc = "Mo
 vim.api.nvim_set_keymap("i", "<A-up>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
 vim.api.nvim_set_keymap("v", "<A-down>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
 vim.api.nvim_set_keymap("v", "<A-up>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
+
+-- Insert blank line up/down cursor position in normal mode
+vim.api.nvim_set_keymap("n", "<leader><cr>", "o<esc>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader><bs>", "O<esc>", { noremap = true })
+
+-- Don't add single character removal to the default registry
+vim.api.nvim_set_keymap("n", "x", '"_x', { noremap = true })
+vim.api.nvim_set_keymap("v", "x", '"_d', {})
+
+-- Remove line/selection without adding it to the default registry
+vim.api.nvim_set_keymap("n", "X", '"_dd', {})
+vim.api.nvim_set_keymap("v", "X", '"_d', {})
+
+-- When changing text, don't add it to default registry
+vim.api.nvim_set_keymap("n", "c", '"_c', { noremap = true })
+vim.api.nvim_set_keymap("v", "c", '"_c', { noremap = true })
+vim.api.nvim_set_keymap("n", "C", '"_C', { noremap = true })
+vim.api.nvim_set_keymap("v", "C", '"_C', { noremap = true })
+
+-- " Make Y consistent with C and D
+vim.api.nvim_set_keymap("n", "Y", "y$", { noremap = true })
